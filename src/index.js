5e58e94d46d4af7282a080549c95c16c0331f6ac
Jack Schofield <jackschofield1@googlemail.com>
2022-02-28 15:57:13 +0000

    first test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import "./styles.css";
 
-// STUCTURE CHECKING STAGE YOUR UPLOAINDG FILE WHICH OF YOUR COLUM IS FACILITY NAME ETC, RENAME THOSE SO TEHY DONT PEANLISED
+
+// const Ajv = require("ajv")
+// const addFormats = require("ajv-formats")
+
+// const ajv = new Ajv()
+// addFormats(ajv)
+
+
+// addFormats(ajv, ["date", "int32", "regex" ])
+
+
+
+// const schema = {
+//   type: "string",
+//   format: "date",
+//   formatMinimum: 0,
+//   formatExclusiveMaximum: 5,
+// }
+
+//AJV check that the json has a row called STAR RATING
+
+// STRUCTURE CHECKING STAGE YOUR UPLOADING FILE WHICH OF YOUR COLUM IS FACILITY NAME ETC, RENAME THOSE SO THEY AREN'T PENALISED 
 
 // THEN START ASKING ABOUT THE ACTUAL DATA 
 
 // CSV WILL LOOK AT THE SCHEMA 
-// JSON TEST
+// JSON TEST THE CONTENT
 
 
 // MUST HAVE CSV
@@ -19,12 +40,56 @@ import "./styles.css";
 // AJV CHECK WHETHER THE STRUCTURE IS RIGHT VALUES ARE NUMERICAL OR STRING
 
 
+//IMPORTING SCHEMA FROM LOCAL
+// const validate = ajv.compile(schema)
+
+
+// ajv.addSchema(JustPlacementSchema, "test/schemas/excellent-100-2.json");
+
+
+//CREATING SIMPLE SCHEMA TO TEST
+const schema = {
+  type: "object",
+  properties: {
+      Region: {
+          "type": "string"
+      },
+      District: {
+          "type": "string"
+      },
+      Date: {
+          "type": "string"
+      },
+      HealthFacilityType: {
+          "type": "string"
+      },
+      HealthFacilityOwnership: {
+          "type": "string"
+      },
+      HealthFacilityName: {
+          "type": "string"
+      },
+      StarRating: {
+          "type": "string"
+      },
+  },
+  required: ["foo"],
+  additionalProperties: false
+}
+
+let objTester = {
+    Region: "Region",
+    District: "District",
+    Date: "Date",
+    HealthFacilityType: "HealthFacilityType",
+    HealthFacilityOwnership: "HealthFacilityOwnership",
+    HealthFacilityName: "HealthFacilityName",
+    StarRating: "StarRating",
+}
+const test = ajv.compile(schema);
+const isValid = test (objTester);
+console.log(isValid ? obj : {obj, error: test.errors });
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
+<h1>AUTO-GRADER</h1>
 `;
